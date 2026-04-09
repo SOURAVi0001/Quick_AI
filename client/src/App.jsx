99dd66c8a739033f33d6afc359a8101a4166abcb
SOURAVI0001 <souravpandr@gmail.com>
2026-04-10 04:46:34 +0530

    build updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
     <div>
       <Toaster />
       <Routes>
+      <div>HI</div>
         <Route path="/" element={<Home />} />
         <Route path="/ai" element={<Layout />}>
           <Route index element={<Dashboard />} />
